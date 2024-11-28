29c0d24d503dfb4ba772680789e7c7708bcf73c5
SK <khanalsanjay48@gmail.com>
2024-11-28 13:04:04 +0545

    react local storage

--- src/Components/DynamicField.jsx
+++ src/Components/DynamicField.jsx
@@ -7,9 +7,31 @@ import {
   Stack,
   Flex,
 } from "@chakra-ui/react";
-
-import { useForm, useFieldArray } from "react-hook-form";
+import { useForm, useFieldArray, useWatch } from "react-hook-form";
 import "../App.css";
+import { useState, useEffect } from "react";
+
+// const localStorage = (Quantity, quantity) => {
+//   const [currentQuantity, setQuantity] = useState(() => {
+//     let currentQuantity;
+
+//     try {
+//       currentQuantity = JSON.parse(
+//         localStorage.getItem(Quantity) || String(quantity)
+//       );
+//     } catch (error) {
+//       currentQuantity = quantity;
+//     }
+
+//     return currentQuantity;
+//   });
+
+//   useEffect(() => {
+//     localStorage.setItem(Quantity, JSON.stringify(quantity));
+//   }, [quantity, Quantity]);
+
+//   return [quantity, setQuantity];
+// };
 
 const DynamicField = () => {
   const {
@@ -30,12 +52,20 @@ const DynamicField = () => {
     name: "users", // Name of the field array
   });
 
+  const quantity = useWatch({ control, name: "users.0.quantity" });
+  console.log(quantity);
+
   // console.log(useFieldArray);
 
   const onsubmit = (data) => {
     console.log(data); // Log the form data
     // reset(); // Reset the form after submission
   };
+
+  useEffect(() => {
+    localStorage.setItem("quantity", JSON.stringify(quantity));
+    console.log(localStorage);
+  }, [quantity]);
 
   return (
     <Box
@@ -52,16 +82,21 @@ const DynamicField = () => {
           <Flex flexDirection="column" justifyContent={"space-between"}>
             {fields.map((field, index) => (
               <div key={field.id}>
+                {/* For User Name */}
                 <FormControl id="user name" mb={4}>
                   <FormLabel htmlFor="user name">User Name</FormLabel>
                   <Input
                     {...register(`users.${index}.userName`, {
-                      minLength: 5,
                       required: "User Name is required",
+                      minLength: {
+                        value: 5,
+                        message: "Must have at least five characters",
+                      },
                     })}
                     type="text"
                     placeholder="User Name"
                   />
+                  {/* Errors Alert */}
                   {errors.users?.[index]?.userName && (
                     <p role="alert" style={{ color: "red" }}>
                       {`${errors.users?.[index]?.userName?.message}`}
@@ -69,28 +104,29 @@ const DynamicField = () => {
                   )}
                 </FormControl>
 
+                {/* For Quantity */}
                 <FormControl id="quantity" marginBottom={4}>
                   <FormLabel htmlFor="quantity">Quantity</FormLabel>
                   <Input
                     {...register(`users.${index}.quantity`, {
                       required: "Quantity is required",
+                      pattern: {
+                        value: /^[0-9]+$/,
+                        message: "Quantity must be a valid number",
+                      },
                     })}
                     type="text"
                     placeholder="Quantity"
                   />
+                  {/* Errors Alert */}
                   {errors.users?.[index]?.quantity && (
                     <p role="alert" style={{ color: "red" }}>
                       {`${errors.users?.[index]?.quantity?.message}`}
                     </p>
                   )}
                 </FormControl>
-                <button
-                  className="Add-user"
-                  type="button"
-                  onClick={() => append({})}
-                >
-                  Add User
-                </button>
+
+                {/* Delete for user name & quantity */}
                 <button
                   className="deleted"
                   type="button"
@@ -102,10 +138,12 @@ const DynamicField = () => {
             ))}
           </Flex>
 
-          {/* <button className="Add-user" type="button" onClick={() => append({})}>
+          {/* Add new user name & quantity*/}
+          <button className="Add-user" type="button" onClick={() => append({})}>
             Add User
-          </button> */}
+          </button>
 
+          {/* Login button */}
           <Button colorScheme="blue" type="submit" size="lg">
             login
           </Button>
---
+++ src/Components/UseLocalStorage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { useState } from "react";
+
+const UseLocalStorage = () => {
+  return <div>UseLocalStorage</div>;
+};
+
+export default UseLocalStorage;
